bd6bcdecc39c4a852908e921915e1d7c9cc71a48
Andrei Dicu <107606607+AndreiDicu01@users.noreply.github.com>
2022-11-16 23:56:34 +0200

    Add files via upload

---
+++ ProxyServer-Src/Timer.cpp
@@ -0,0 +1,44 @@
+#include "Timer.hpp"
+
+std::string Timer::getCurrentDateTime(std::string s)
+{
+    time_t now = time(0);
+    struct tm tstruct;
+    char buf[80];
+    tstruct = *localtime(&now);
+
+    if (s == "now")
+    {
+        strftime(buf, sizeof(buf), "%Y-%m-%d %X", &tstruct);
+    }
+    else if (s == "date")
+    {
+        strftime(buf, sizeof(buf), "%Y-%m-%d", &tstruct);
+    }
+
+    return std::string(buf);
+}
+// copiem in buf prin formatele de mai sus , contentul aflat in tstruct - local time current
+
+double Timer::getCurrentSec()
+{
+    return time(0);
+}
+// pur si simplu returnam timpul curent ca numar
+
+std::string Timer::getlocalTimeStr(long timeNum)
+{
+    if (timeNum > this->LimMax)
+    {
+        return "Never Expire";
+    }
+    else
+    {
+        time_t rawtime = timeNum;
+        struct tm *timeinfo;
+        timeinfo = localtime(&rawtime);
+        std::string timeStr(asctime(timeinfo));
+        return timeStr.substr(0, timeStr.size() - 1);
+    }
+}
+//convertor number --> string in cazul in care expira timpul
